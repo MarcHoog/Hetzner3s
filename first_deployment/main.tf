f2abcfe47a78c302098e070ae279c1705960221c
Marc Hoogendoorn <Marc.Hoogendoorn@simac.com>
2025-05-01 00:11:30 +0200

    working on first deployment

--- first_deployment/main.tf
+++ first_deployment/main.tf
@@ -1,14 +1,34 @@
 terraform {
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~> 1.50.1"
+    }
+  }
+  required_version = ">= 1.6, < 2.0"
+
   backend "s3" {
-    bucket = "mybucket"
-    key    = "path/to/my/key"
-    region = "us-east-1"
+    bucket  = "myterraform"
+    key     = "first_deployment/state/state.tfstate"
+    region  = "hetzner"      
+    endpoints = {
+      s3 = "https://nbg1.your-objectstorage.com"
+    }
+    # Common flags for compatibility:
+    skip_credentials_validation = true   # this will skip AWS related validation
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true   # skips checking STS 
+    use_path_style              = true   # Ceph-S3 compatibility
+    skip_s3_checksum            = true  
+    
   }
 }
 
-
 module "hetzner_server_storage_firewall" {
-  source        = "../../modules/hetzner_server_storage_firewall"
+  
+  source        = "git::ssh://git@github.com/MarcHoog/MyTerraformModules.git//modules/hetzner/app_S1vF?ref=main"
+  
   server_name   = "pilot-server-prod"
   image         = "ubuntu-22.04"         # Use a valid image slug for Hetzner Cloud
   server_type   = "cx31"
