eccf5dbeb4014557066f844acaab839785126f89
Marc Hoogendoorn <bubble@mac.home>
2025-05-21 20:15:45 +0200

    ran terraform recursive

--- first_deployment/main.tf
+++ first_deployment/main.tf
@@ -1,39 +1,39 @@
 terraform {
 
-    required_version = ">= 1.5.0"
-    required_providers {
-      hcloud = {
-        source  = "hetznercloud/hcloud"
-        version = ">= 1.49.1"
-      }
+  required_version = ">= 1.5.0"
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = ">= 1.49.1"
     }
+  }
 
 
   backend "s3" {
-    bucket  = "bucketofbuckets"
-    key     = "first_deployment/state/state.tfstate"
-    region  = "digitalocean"      
+    bucket = "bucketofbuckets"
+    key    = "first_deployment/state/state.tfstate"
+    region = "digitalocean"
     endpoints = {
       s3 = "https://ams3.digitaloceanspaces.com"
     }
     # Common flags for compatibility:
-    skip_credentials_validation = true   # this will skip AWS related validation
+    skip_credentials_validation = true # this will skip AWS related validation
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    skip_requesting_account_id  = true   # skips checking STS 
-    use_path_style              = true   # Ceph-S3 compatibility
-    skip_s3_checksum            = true  
-    
+    skip_requesting_account_id  = true # skips checking STS 
+    use_path_style              = true # Ceph-S3 compatibility
+    skip_s3_checksum            = true
+
   }
 }
 
 module "hetzner_server" {
-  
-  source        = "git::ssh://git@github.com/MarcHoog/MyTerraformModules.git//modules/hetzner/server?ref=main"
-  server_names  = ["dingdong"]
-  image         = "ubuntu-22.04"         # Use a valid image slug for Hetzner Cloud
-  server_type   = "cx22"
-  location      = "nbg1"                 # Example location; adjust as needed
-  nodes         = 1
-  ipv6_enabled  = true
+
+  source       = "git::ssh://git@github.com/MarcHoog/MyTerraformModules.git//modules/hetzner/server?ref=main"
+  server_names = ["dingdong"]
+  image        = "ubuntu-22.04" # Use a valid image slug for Hetzner Cloud
+  server_type  = "cx22"
+  location     = "nbg1" # Example location; adjust as needed
+  nodes        = 1
+  ipv6_enabled = true
 }
